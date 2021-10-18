11dc3e69a689ec311d19d9208fa4e647d99b7223
Victor Velasco <victor.velasco@student.swic.edu>
2021-10-17 19:51:57 -0500

    10-17

---
+++ Homework/SavingsAccount/SavingsAccount/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SavingsAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Start two savings accounts
+            SavingsAccount saver1 = new SavingsAccount(2000);
+            SavingsAccount saver2 = new SavingsAccount(3000);
+
+            AccountTitle("Interest rate at 4%");
+            DisplayAccounts(4); // Interest rate 4%
+
+            AccountTitle("After increasing rate to 5%");
+            DisplayAccounts(5); // Interest rate 5%
+
+            void IncreaseRate(double r)
+            {
+                SavingsAccount.AnnualInterestRate = r;
+            }
+
+            void DisplayAccounts(int r)
+            {
+                /*
+                 *     Account 1
+                 */
+                Console.WriteLine($"Savings account #1:\n\tPrevious Balance: {saver1.SavingsBalance,-10:C}");
+                
+                // Increase rate
+                IncreaseRate(r);
+                saver1.CalculateMonthlyInterest(); // Calculate interest
+
+                Console.WriteLine($"\tInterest Accrued: {saver1.AccruedInterest,-10:C}" +
+                $"\n\tCurrent Balance:  {saver1.SavingsBalance,-10:C}");
+                
+                /* 
+                 *     Account 2
+                 */
+                Console.WriteLine($"\nSavings account #2:\n\tPrevious Balance: {saver2.SavingsBalance,-10:C}");
+
+                // Increase rate
+                IncreaseRate(r);
+                saver2.CalculateMonthlyInterest(); // Calculate interest
+
+                Console.WriteLine($"\tInterest Accrued: {saver2.AccruedInterest,-10:C}" +
+                $"\n\tCurrent Balance:  {saver2.SavingsBalance,-10:C}");
+            }
+
+            void AccountTitle(string m)
+            {
+                Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                Console.WriteLine($"\n{m}\n".ToUpper());
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+
+            // Exit
+            Console.Write("\nPRESS ANY KEY TO EXIT: ");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homework/SavingsAccount/SavingsAccount/SavingsAccount.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SavingsAccount
+{
+    class SavingsAccount
+    {
+        // Constructor
+        public SavingsAccount(double b)
+        {
+            SavingsBalance = b;
+        }
+
+        // Properties
+        private static double annualInterestRate;
+        public static double AnnualInterestRate
+        {
+            get
+            {
+                return annualInterestRate;
+            }
+            set
+            {
+                annualInterestRate = value;
+            }
+        }
+
+        private double savingsBalance;
+        public double SavingsBalance
+        {
+            get
+            {
+                return savingsBalance;
+            }
+            set
+            {
+                if (value < 0)
+                    savingsBalance = 0;
+                else
+                    savingsBalance = value;
+            }
+        }
+
+        public double AccruedInterest { get; set; }
+
+        // Method
+        public void CalculateMonthlyInterest()
+        {
+            AccruedInterest = (SavingsBalance * (AnnualInterestRate / 100)) / 12;
+            SavingsBalance += AccruedInterest;
+        }
+    }
+}
