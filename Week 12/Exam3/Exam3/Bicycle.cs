b7f6e9d23acaf7c4c6a3d46c2c19496063526b19
Victor Velasco <victor.velasco@student.swic.edu>
2021-11-15 20:13:24 -0600

    10/15

---
+++ Week 12/Exam3/Exam3/Bicycle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam3
+{
+    class Bicycle : Vehicle
+    {
+        public double Weight { get; set; }
+
+        public override double Speed { get => 2000 / Weight; }
+
+        public Bicycle(string n, double mi, double w) : base(n, mi) 
+        {
+            Weight = w;
+        }
+    }
+}
---
+++ Week 12/Exam3/Exam3/PHEV.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam3
+{
+    class PHEV : Auto
+    {
+        public override double Cost { get => (Miles - Range) / MPG * 4.00; }
+
+        public double Range { get; set; }
+
+        public PHEV(string n, double mi, double m, double r) : base(n, mi, m)
+        {
+            Range = r;
+        }
+    }
+}
